ed59620d844b17bc79ff828ebeaa61303cbe9fa6
Lê Công Minh Khôi <lcngmnhkhoi@gmail.com>
2023-07-11 10:56:04 +0700

    default orient

--- server/web/api/indicator/schema.py
+++ server/web/api/indicator/schema.py
@@ -1,4 +1,5 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
+from typing import Optional
 from datetime import datetime, timedelta
 
 
@@ -13,10 +14,16 @@ class IndicatorDTOModel(BaseModel):
     indicator: str
     from_date: str
     to_date: str
-    orient: str
+    orient: Optional[str]
 
     class Config:
         orm_mode = False
+
+    @validator("orient", pre=True, always=True)
+    def set_default_orient(cls, orient):
+        if orient is None:
+            return "records"
+        return orient
 
 
 # class IndicatorOutputDTOModel(BaseModel):
